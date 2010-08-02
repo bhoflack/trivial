aa42454c016a5334a0235b482d1f2227826a95ea
Brecht Hoflack <brecht.hoflack@gmail.com>
2010-08-02 22:24:02 +0200

    Added make-schema.

--- trivial.lisp
+++ trivial.lisp
@@ -1,2 +1,34 @@
 (in-package :trivial.db)
 
+(defclass object ()
+  ((id :initarg :id :reader id)))
+
+(defclass tree (object)
+  ((items :initarg :items :reader items)))
+
+(defclass value (object)
+  ((name  :initarg :name  :reader name)
+   (value :initarg :value :reader value)))
+				    
+(defgeneric make-column (name type &key default-value value))
+
+(defmethod make-column (name (type (eql 'string)) &key default-value value)
+  (make-instance 'value
+		 :name name
+		 :value value))
+
+(defmethod make-column (name (type (eql 'tree)) &key default-value value)
+  (make-instance 'tree))
+
+(defun make-schema (spec)
+  (mapcar #'(lambda (column-spec) (apply #'make-column column-spec)) spec))
+    				   
+	       	       		   
+			       	    
+			       	      
+				       	  
+     				       	   
+    
+
+
+
